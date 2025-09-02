11d2a420408af9147eb87c164f57ad31f7abb9e5
Harmornic <chekge.88899@gmail.com>
2025-09-03 04:28:01 +0700

    final

--- video_stream_player.gd
+++ video_stream_player.gd
@@ -1,1 +1,12 @@
 extends VideoStreamPlayer
+
+@export var next_scene: String = "res://main_menu.tscn"
+
+func _ready() -> void:
+	# ถ้ามีวิดีโออยู่ใน stream และ autoplay ถูกเปิดไว้
+	if stream:
+		play()
+		connect("finished", Callable(self, "_on_video_finished"))
+
+func _on_video_finished():
+	get_tree().change_scene_to_file(next_scene)
