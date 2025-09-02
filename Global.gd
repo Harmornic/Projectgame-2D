9c3da1eef4ca4e2aa754e7afde2a572422387502
Harmornic <chekge.88899@gmail.com>
2025-09-02 22:59:54 +0700

    10:59

--- Global.gd
+++ Global.gd
@@ -2,4 +2,5 @@ extends Node
 
 var has_key: bool = false
 var has_axe: bool = false
-var has_clicked_stone: bool = false  # ✅ ต้องมีบรรทัดนี้!
+var has_clicked_stone: bool = false
+# เพิ่มตัวอื่นได้ตามต้องการ เช่น has_opened_door, has_cut_wood ฯลฯ
