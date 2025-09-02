299cb592b56209d3a0d0fdd15b681f905a7b4a25
naruset666 <naruset.a@kkumail.com>
2025-09-03 01:09:57 +0700

    ประตูเรืองแสง

--- exittrue.gd
+++ exittrue.gd
@@ -3,7 +3,7 @@ extends Area2D
 @export var normal_tex: Texture2D # Texture ปกติ
 @export var hover_tex: Texture2D  # Texture เมื่อเมาส์ชี้
 
-@onready var sprite: Sprite2D = $Puzzledoor # แก้ไขพาธตรงนี้
+@onready var sprite: Sprite2D = $Trueexit # แก้ไขพาธตรงนี้
 
 func _ready() -> void:
 	if normal_tex:
@@ -18,7 +18,3 @@ func _on_mouse_entered() -> void:
 func _on_mouse_exited() -> void:
 	if normal_tex: # ตรวจสอบว่ามี normal_tex กำหนดไว้
 		sprite.texture = normal_tex
-
-func _input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		get_tree().change_scene_to_file("res://puzzleroom.tscn")
